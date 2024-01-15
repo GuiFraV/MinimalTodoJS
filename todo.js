9e354d42d3a7a3d034d69081880906f36c571b4d
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-15 16:43:34 +0100

    add style

--- todo.js
+++ todo.js
@@ -2,3 +2,8 @@ const taskInput = document.querySelector('#taskInput');
 const taskList = document.querySelector("#taskList");
 const addTaskeForm = document.querySelector('#addTaskForm');
 
+taskList.style= `
+    list-style:none;
+    margin-top: 1rem;
+    font-size: 1.5rem;
+`
