4a5cf36598932b1b5b0c34198c96a94ab0761bae
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-15 16:47:06 +0100

    createTaskItem function

--- todo.js
+++ todo.js
@@ -7,3 +7,12 @@ taskList.style= `
     margin-top: 1rem;
     font-size: 1.5rem;
 `
+const createTaskItem = (task) => {
+    `
+        <li>
+            <input type="checkbox" name="task" value="${task}" />
+            <label for"task">${task}</label>
+            <button type="button">X</button>
+        </li>
+    `
+}
