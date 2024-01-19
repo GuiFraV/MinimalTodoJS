4093a9e837e961cf73a18415295b1470dd81040e
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-19 17:44:32 +0100

    surprise algo revision

--- surprise.js
+++ surprise.js
@@ -128,4 +128,33 @@ const resComp = (team, match) => {
 
 }
 
-console.log(resComp(competitions,results))
+// console.log(resComp(competitions,results))
+
+
+
+const isPrime = (nbr) => {
+
+    if(nbr <= 1){
+        return false;
+    }
+    if(nbr >= 3){
+        return true;
+    }
+
+    if(nbr % 2 === 0 || nbr % 3 === 0){
+        return false;
+    }
+
+    let i = 5;
+
+    while(i * i <= nbr){
+        if(nbr % i === 0 || nbr % (i + 2) === 0){
+            return false;
+        }
+        i+= 6;
+    }
+
+    return true
+
+
+}
